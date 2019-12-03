f23b0ffe5ea8e8d364424b71e4518a9dc3393cca
Mahmoud Rateb <mr@shopbox.com>
2019-12-03 15:33:47 +0200

    Removing unnecessary extras from routes + deleting unnecessary files from docker-data

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'chats/create'
-  get 'chats/update'
-  get 'chats/show'
-  get 'chats/index'
-  # get 'applications/create'
-  # get 'applications/update'
-  # get 'applications/show'
 
   resources :applications, only: [:create,:update,:show] do
   	resources :chats, only: [:create, :update, :index, :show] do 
@@ -14,5 +7,5 @@ Rails.application.routes.draw do
       end
     end
   end 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
